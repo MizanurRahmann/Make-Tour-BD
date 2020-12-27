3d06c3651ca3962b13c93d5f065c01be0766dcbc
MizanurRahmann <mr926560@gmail.com>
2020-12-27 12:38:34 +0600

    connect local db with mongoose.

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const app = require("./app");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config({ path: "./config.env" });
-console.log(process.env);
 
+// Local db path
+const DB = process.env.DATABASE_LOCAL;
+
+// Connect with local db using mongoose
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: true,
+    })
+    .then((con) => {
+        console.log("DB connection successful! 📈");
+    });
+
+// App runner with specific port
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`);
+    console.log(`\nApp running on port ${port}. 👋`);
 });
