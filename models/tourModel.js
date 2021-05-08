f4973101ee00c0619ca0583b71c6069db5e71fa7
MizanurRahmann <mr926560@gmail.com>
2021-05-08 22:46:20 +0600

    ADD: aggregation middleware in Tour model.

--- models/tourModel.js
+++ models/tourModel.js
@@ -92,6 +92,14 @@ tourSchema.post(/^find/, function(docs, next) {
      next();
  });
 
+// AGGREGATION MIDDLEWARE:
+// not include secrete tours..
+tourSchema.pre('aggregate', function(next) {
+    this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+    next();
+});
+
+
 const Tour = mongoose.model("Tour", tourSchema);
 
 module.exports = Tour;
