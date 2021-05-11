7041c1e398a1f2dfa374fab6ff12fb3c7aa486a4
MizanurRahmann <mr926560@gmail.com>
2021-05-11 12:09:03 +0600

    HANDLE: unhandling routes.

--- app.js
+++ app.js
@@ -6,9 +6,17 @@ const userRouters = require("./routers/userRouters");
 // MIDDLEWARE
 const app = express();
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 // ROUTERS
 app.use("/api/v1/tours", tourRouters);
 app.use("/api/v1/users", userRouters);
+
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server.`
+    })
+})
 
 module.exports = app;
