666bb2e9e0849896510d2115148732b16145c929
MizanurRahmann <mr926560@gmail.com>
2021-05-18 08:10:13 +0600

    set error for: duplicate database fields.

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,12 @@ const handleCastError = (err) => {
   return new AppError(message, 400);
 }
 
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value ${value}. Please another value`;
+  return new AppError(message, 400);
+}
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -40,8 +46,10 @@ module.exports = (err, req, res, next) => {
   } else if(process.env.NODE_ENV === 'production'){
     let error = {...err};
     
-    // Invalid database id
+    // Invalid database id (Cast error)
     if(err.name === 'CastError') error = handleCastError(error);
+    // Duplicate Database fields
+    if(err.code === 11000) error = handleDuplicateFieldsDB(error);
 
     sendErrorProd(error, res);
   }
