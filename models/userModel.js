5ea9dddec6492581580dfe6da79ce589461123a9
MizanurRahmann <mr926560@gmail.com>
2021-05-20 13:11:58 +0600

    MANAGE: user password.

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema({
     name: {
@@ -17,13 +18,27 @@ const userSchema = mongoose.Schema({
     password: {
         type: String,
         required: [true, "Please provide a password."],
-        maxlength: [6, 'Password should be greater than 6 character.']
+        minlength: [6, 'Password should be greater than 6 character.']
     },
     confirmPassword: {
         type: String,
         required: [true, "Please confirm your password."],
+        validate: {
+            validator: function(el) { return el === this.password; },
+            message: 'Passwords should be matched.'
+        }
     }
-})
+});
+
+userSchema.pre('save', async function(next) {
+    // Only run this function if password was actually modified
+    if(!this.isModified('password')) return next();
+
+    // Hash the password with cost 12 and delete confirmPasswor
+    this.password = await bcrypt.hash(this.password, 12);
+    this.confirmPassword = undefined;
+    next();
+});
 
 const User = mongoose.model("User", userSchema);
 
