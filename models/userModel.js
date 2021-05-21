db20bdb7673cfeac3db4515526df4d8e4509a114
MizanurRahmann <mr926560@gmail.com>
2021-05-21 14:31:36 +0600

    ADD: JWT token based login.

--- models/userModel.js
+++ models/userModel.js
@@ -3,42 +3,52 @@ const validator = require("validator");
 const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "A user must have a name."],
+  name: {
+    type: String,
+    required: [true, "A user must have a name."],
+  },
+  email: {
+    type: String,
+    required: [true, "A user must have a email."],
+    validate: [validator.isEmail, "Please provide a valid email."],
+    unique: true,
+    lowercase: true,
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, "Please provide a password."],
+    minlength: [6, "Password should be greater than 6 character."],
+    select: false,
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, "Please confirm your password."],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords should be matched.",
     },
-    email: {
-        type: String,
-        required: [true, "A user must have a email."],
-        validate: [validator.isEmail, 'Please provide a valid email.'],
-        unique: true,
-        lowercase: true,
-    },
-    photo: String,
-    password: {
-        type: String,
-        required: [true, "Please provide a password."],
-        minlength: [6, 'Password should be greater than 6 character.']
-    },
-    confirmPassword: {
-        type: String,
-        required: [true, "Please confirm your password."],
-        validate: {
-            validator: function(el) { return el === this.password; },
-            message: 'Passwords should be matched.'
-        }
-    }
+  },
 });
 
-userSchema.pre('save', async function(next) {
-    // Only run this function if password was actually modified
-    if(!this.isModified('password')) return next();
+userSchema.pre("save", async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified("password")) return next();
 
-    // Hash the password with cost 12 and delete confirmPasswor
-    this.password = await bcrypt.hash(this.password, 12);
-    this.confirmPassword = undefined;
-    next();
+  // Hash the password with cost 12 and delete confirmPasswor
+  this.password = await bcrypt.hash(this.password, 12);
+  this.confirmPassword = undefined;
+  next();
 });
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model("User", userSchema);
 
--- routers/userRouters.js
+++ routers/userRouters.js
@@ -5,6 +5,7 @@ const authController = require("../controllers/authController");
 const userRouters = express.Router();
 
 userRouters.post("/signup", authController.signup);
+userRouters.post("/login", authController.login);
 
 userRouters
   .route("/")
