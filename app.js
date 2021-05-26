4f012fcac85d85fec01615cdfa1a84202b5bc3c8
MizanurRahmann <mr926560@gmail.com>
2021-05-26 10:23:50 +0600

    Preventing parameter pollution.

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
-const xss =  require('xss-clean');
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -12,24 +13,38 @@ const userRouters = require("./routers/userRouters");
 // ------- MIDDLEWARES ----------
 const app = express();
 // security http headers
-app.use(helmet())
+app.use(helmet());
 
 // security rate limit
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour.',
+  message: "Too many requests from this IP, please try again in an hour.",
 });
-app.use('/api',limiter);
+app.use("/api", limiter);
 
 // body perser, reading data from body into req.body
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: "10kb" }));
 
 // data sanitization against NoSQL query injection
 app.use(mongoSanitize());
 
 // data sanitization against xss
 app.use(xss());
+
+// prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "price",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
 
 // serving static files
 app.use(express.static(`${__dirname}/public`));
