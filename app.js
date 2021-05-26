22295581435a8f389503b4f8080bc37490a55d09
MizanurRahmann <mr926560@gmail.com>
2021-05-26 10:05:07 +0600

    Setting secuirity http headers.

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const express = require("express");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const tourRouters = require("./routers/tourRouters");
 const userRouters = require("./routers/userRouters");
 
-// MIDDLEWARE
+// ------- MIDDLEWARES ----------
 const app = express();
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
-
+// security http headers
+app.use(helmet())
+// security rate limit
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour.',
 });
 app.use('/api',limiter);
+// body perser, reading data from body into req.body
+app.use(express.json());
+// serving static files
+app.use(express.static(`${__dirname}/public`));
 
-// ROUTERS
+// ---------- ROUTERS ----------
 app.use("/api/v1/tours", tourRouters);
 app.use("/api/v1/users", userRouters);
 
@@ -26,7 +31,7 @@ app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server.`, 404));
 });
 
-// GLOBAL ERROR HANDLING MIDDLEWARE
+// ---------- GLOBAL ERROR HANDLING MIDDLEWARE ----------
 app.use(globalErrorHandler);
 
 module.exports = app;
