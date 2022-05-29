30a6627d4e9ccd7721fb66241b373902b8b9b4b6
MizanurRahmann <mizanur.rahman03032@gmail.com>
2022-05-30 00:23:25 +0600

    ADD: nested routes for reviews -  with mergeParams.

--- routers/tourRouters.js
+++ routers/tourRouters.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const tourController = require("../controllers/tourController");
 const authController = require("../controllers/authController");
+const reviewRouter = require("./reviewRoutes");
 const router = express.Router();
+
+
+router.use("/:tourId/reviews", reviewRouter);
 
 router
   .route("/top5-cheap")
