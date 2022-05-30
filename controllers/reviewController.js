2fd83673b14c8e46b8d4e2c18f59ba38106ca72f
MizanurRahmann <mizanur.rahman03032@gmail.com>
2022-05-31 02:20:11 +0600

    ADD: new features to review controllers.

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -18,6 +18,22 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
     });
 });
 
+exports.getReview = catchAsync(async (req, res, next) => {
+  const id = req.params.id;
+  const review = await Review.findById(id);
+
+  if(!review) {
+    return next(new AppError('No review found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      review,
+    },
+  });
+});
+
 exports.createReview = catchAsync(async (req, res, next) => {
     if(!req.body.tour) req.body.tour = req.params.tourId;
     if(!req.body.user) req.body.user = req.user.id;
@@ -30,4 +46,32 @@ exports.createReview = catchAsync(async (req, res, next) => {
           review: newReview,
         },
       });
+})
+
+
+exports.deleteReview = catchAsync(async (req, res, next) => {
+  const review = await Review.findByIdAndDelete(req.params.id);
+  if(!review){
+    return next(new AppError('No review found with this ID', 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: null,
+  });
+});
+
+
+exports.updateReview = catchAsync(async (req, res, next) => {
+  const review = await Review.findByIdAndUpdate(req,params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      review
+    },
+  });
 })
